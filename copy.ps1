0c5420d6ba92a8d4c5f2d8a9c841313353e3fed3
vasanthshanmugam <32473491+vasanthshanmugam@users.noreply.github.com>
2023-02-03 19:12:19 +0000

    Update copy.ps1

--- copy.ps1
+++ copy.ps1
@@ -1,5 +1,5 @@
-$source = "C:\Vasanth\AzureDevOpsDemo\Source"
-$destination = "C:\Vasanth\AzureDevOpsDemo\Destination"
+$source = "C:\AzureDevOpsDemo\Source"
+$destination = "C:\AzureDevOpsDemo\Destination"
 
 Get-ChildItem $source -Recurse | ForEach-Object {
     $destinationPath = $_.FullName.Replace($source, $destination)
